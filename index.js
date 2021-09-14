f5cc282bd79ec6b9950df7c275209a6ee4707c4d
ryliespriggs <rspriggs812@gmail.com>
2021-09-14 16:50:26 -0700

    commit tree 5f663127a7ea3c5f0c42aaffd9b27e853fcff4d0

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
 // Write your code here
+
+//multiply 
+const num1 = 2
+const num2 = 31
+let multiply = num1 * num2
+
+//random with math.ciel
+let random = Math.ceil(Math.random() * 5) + 1
+
+//remainder
+const num3 = 16
+const num4 = 6
+let mod = num3 % num4
+
+//max
+let max = Math.max(2, 4, 6, 20)
+
